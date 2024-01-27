e55d708538d28b1f169ed6391a516c2d05b44eed
Raghav Sharma <redclaww02@gmail.com>
2024-01-27 19:58:38 +0530

    Added Components

--- src/components/BrandInput.tsx
+++ src/components/BrandInput.tsx
@@ -1,11 +1,13 @@
-"use client"
+
 import React, { useEffect } from "react";
 import { useState } from "react";
 import { useRecoilState } from "recoil";
 
 export default async function page(){
 
-  const [brandlist, setBrandlist] = useState('Apple');
+
+
+  const [brandlist, setBrandlist] = useState(['Apple']);
   const [device, setDevice] = useState('');
   const data = ['Hello'];
      useEffect(() => {
@@ -19,13 +21,13 @@ export default async function page(){
     >
       Brands
     </label>
-    <select
+    <select defaultValue={"none"}
     value={brandlist}
     onChange={(e) => setBrandlist(e.target.value)}
       id="countries"
       className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
     >
-      <option defaultValue={"none"}>Choose a Brand</option>
+      <option >Choose a Brand</option>
       {data.map((brand: any) => (
         <option value={brand} >{brand}</option>
     ))}
