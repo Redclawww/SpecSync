d1050344ca7f32e017f509805206821a9f0a60e6
Raghav Sharma <redclaww02@gmail.com>
2024-01-27 19:37:39 +0530

    Imported code to React

--- src/app/api/phonelist/route.ts
+++
@@ -1,8 +0,0 @@
-import gsmarena from "gsmarena-api"
-
-export async function GET() {
-  const devices = await gsmarena.catalog.getBrand("apple-phones-48");
-  const name = devices.map((device: any) => device.name);
-   
-    return Response.json({ name })
-  }
