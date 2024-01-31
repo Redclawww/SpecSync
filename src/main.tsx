6c920970d9c46b36c0e5b782aaf94c1da8d3e2ac
Priyasha Shrivastava <tashishrivastava77249@gmail.com>
2024-02-01 00:46:55 +0530

    Google Auth done

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { GoogleOAuthProvider } from '@react-oauth/google'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId="<805661459359-o27ntc5t0sp4271r8dlska7tegevpep6.apps.googleusercontent.com>">
+    <GoogleOAuthProvider clientId="219749234928-1e9mh1of3hi4tblj0a7qu5nij3g1973j.apps.googleusercontent.com">
     <App />
       </GoogleOAuthProvider>
   </React.StrictMode>,
--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -7,15 +7,10 @@ export const Login = () => {
     <div className=" bg-black h-screen w-screen">
     <video className= "w-full h-full object-cover" muted autoPlay loop>
     <source src='Videos/video.mp4' type='video/mp4'/>
+    
     </video>
-    <div className="flex flex-col">
-    <button className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 transition duration-300 ease-in-out px-5 py-5 pl-5 border-none rounded-2xl shadow-2xl bg-[#0B0D1B] text-white text-6xl font-semibold leading-tight bg-no-repeat bg-left-center my-2">
-    Sign in with Google</button>
-    </div>
-    </div>
-
-  
-    {/* <GoogleLogin
+    <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 transition duration-300 ease-in-out px-10 py-5 pl-5 border-none rounded-2xl shadow-2xl text-white text-6xl font-semibold leading-tight bg-no-repeat bg-left-center my-2">
+    <GoogleLogin 
         onSuccess={(credentialResponse) => {
           if (credentialResponse?.credential) {
             const decoded = jwtDecode(credentialResponse.credential);
@@ -27,7 +22,17 @@ export const Login = () => {
         onError={() => {
           console.log('Login Failed');
         }}
-      /> */}
+      />
+    </div>
+    <div className="flex flex-col">
+    
+    {/* <button className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 transition duration-300 ease-in-out px-5 py-5 pl-5 border-none rounded-2xl shadow-2xl bg-[#0B0D1B] text-white text-6xl font-semibold leading-tight bg-no-repeat bg-left-center my-2">
+    Sign in with Google</button> */}
+    </div>
+    </div>
+
+  
+   
     
     </>
   )
