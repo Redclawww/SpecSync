1bf7cf6bd083033c3e67e1433299da47a0e65c37
Raghav Sharma <redclaww02@gmail.com>
2024-01-31 19:44:14 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,12 +3,12 @@ import { Link } from "react-router-dom"
 export const Navbar = () => {
   return (
     <>
-    <div className="absolute  flex justify-around text-white text-3xl w-screen pt-4 pb-4 ">
-    <Link to={'/'}>SpecSync</Link>
-      <div className="w-72 h-10 ring ring-gray-500 rounded-lg flex justify-center">
+    <div className="absolute flex justify-around text-white text-3xl w-screen pt-4 pb-4 ">
+      <Link to={'/'}>SpecSync</Link>
+      <div className=" border border-gray-200 rounded-[25px] flex justify-center gap-20 py-3 px-7 text-lg font-semibold">
       <Link to={'/about'}>About</Link>
-      </div>
-      <Link to={'/login'}>Login</Link>
+      <Link to={'/login'}>Login/Signup</Link>
+      </div>   
     </div> 
     </>
     
