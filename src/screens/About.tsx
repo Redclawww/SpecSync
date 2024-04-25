4bffbe509e75e817f0d3028b1b34bd2f2c29029f
Raghav Sharma <redclaww02@gmail.com>
2024-04-26 00:38:11 +0530

    deploy

--- src/screens/About.tsx
+++ src/screens/About.tsx
@@ -12,26 +12,24 @@ export const About = () => {
     <Navbar />
       {" "}
       <div className="bg-[#C2C2C4] w-screen">
-        <div className="h-screen w-screen bg-cover bg-center">
-          <div className="">
-            <h1 className="text-5xl sm:text-5xl md:text-6xl lg:text-7xl flex flex-col pt-52 text-left pl-20 sm:pl-32 md:pl-32 lg:pl-36 xl:pl-36 font-mono font-bold text-black">
+        <div className=" w-screen bg-cover bg-center">
+          <div className="w-screen bg-cover bg-center sm:w-full md:w-full h-screen py-24 flex flex-col justify-center" 
+          style={{ backgroundImage: `url(${ip})` }}>
+            <div className="glass2 w-fit p-10 lg:ml-24 xl:ml-44 md:ml-10 ">
+            <h1 className="text-5xl sm:text-5xl md:text-6xl lg:text-7xl flex flex-col  text-left  font-mono font-bold text-black ">
               About Us
             </h1>
-            <p className="text-black sm:justify-center  font-mono sm:text-2xl md:text-2xl lg:text-2xl pl-14 xl:pl-36 font-semibold pt-8 leading-relaxed">
+            <p className="text-black sm:justify-center  font-mono sm:text-2xl md:text-2xl lg:text-2xl  font-semibold leading-relaxed ">
               SpecSync is your Personal Phone Matchmaker!
               <br /> Discover the perfect mobile phone tailored to
               <br /> your preferences and needs.
               <br /> We compare, you choose –<br /> because your ideal phone is
               just a sync away!
             </p>
+            </div>
           </div>
-          <div className="h-screen w-screen bg-cover bg-center sm:w-full md:w-full lg:w-full sm:h-full md:h-full lg:h-full" 
-          style={{ backgroundImage: `url(${ip})` }}>
-
-          </div>
-
-          <div className="bg-black w-screen h-screen xl:h-max">
-            <p className="text-white text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-semibold text-center pb-20 md:pb-16 p-14 lg:pl-36 xl:pl-36 text-wrap">
+          <div className="bg-black w-screen p-5 md:p-16 lg: xl:h-max">
+            <p className="text-white text-3xl sm:text-2xl md:text-3xl lg:text-6xl font-semibold text-center text-wrap">
               Discover a new era in mobile phone selection with SpecSync. Our
               advanced platform streamlines comparisons by tailoring
               recommendations to your preferences, budget, and needs. Say
@@ -40,31 +38,28 @@ export const About = () => {
             </p>
           </div>
         </div>
-        <div className="h-96"></div>
-        <div className="h-screen"></div>
-        <div className="h-screen w-screen flex justify-center">
+        <div className="w-screen flex justify-center flex-wrap h-fit">
           <video
-            className="w-96 sm:w-[682.667px] mt-18 sm:mt-36 xl:mt-28 h-96 flex"
+            className="w-[80vw] sm:w-[682.667px] mt-18 sm:mt-36  xl:mt-28 h-96 flex"
             muted
             autoPlay
             loop
             src={vid1}
           />
-          <p></p>
-          <video className="w-96 sm:w-[682.667px] mt-96 sm:ml-7 h-96 " 
+          <video className="w-[80vw] sm:w-[682.667px]  xl:mt-96 sm:ml-7 h-96 " 
           muted 
           autoPlay 
           loop 
           src={vid2} />
         </div>
-        <div className="relative p-10">
-          <h1 className="text-6xl font-bold text-black text-start pl-32 ">
+        <div className=" md:p-10 px-5 py-5" >
+          <h1 className="md:text-6xl text-3xl font-bold text-black text-start md:pl-32 ">
             Unbiased Comparison
           </h1>
-          <h1 className="text-6xl font-bold text-[#0884FB] text-center py-10">
+          <h1 className="md:text-6xl text-3xl font-bold text-[#0884FB] md:text-center  py-10">
             Smart Recommendations
           </h1>
-          <h1 className="text-6xl font-bold text-black text-end pr-44">
+          <h1 className="md:text-6xl text-3xl font-bold text-black md:text-end  pr-44">
             Latest Updates
           </h1>
         </div>
